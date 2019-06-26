a4966f9d098911e61e66f062c442e71ba86a5e91
Leia Sousa <leiassousa2008@gmail.com>
2019-06-26 14:25:11 -0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
+#Views são apenas funções Python um pouco mais complicadas
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
